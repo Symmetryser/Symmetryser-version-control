5834385f6a86862f99c188954be972308b204e85
Neukirchner László <neukirchner.laszlo@virt.uni-pannon.hu>
2014-10-29 11:07:50 +0100

    Számítás két metszetpont esetén
    Kijavítottam néhány hibát és olvashatóbbá tettem a függvényt, sajnos még
    mindig nem működik megfelelően.

--- Geometry/Twodotintersect.m
+++ Geometry/Twodotintersect.m
@@ -1,21 +1,21 @@
 function [ x4,y4 ] = Twodotintersect(Phase_1_ex,Amplitude_1_ex,Phase_2_ex,Amplitude_2_ex)
-
-%   Only triangles (FOR NOW)
+% Only triangles 
 [x1,y1] = pol2cart(Phase_1_ex,Amplitude_1_ex);
 [x2,y2] = pol2cart(Phase_2_ex,Amplitude_2_ex);
 Intersection = poly2poly([x1;y1],[x2;y2]);
+x3 = Intersection(1,:);
+y3 = Intersection(2,:);
+% If it is not a two point intersection, throw an error
 Psize=size(x1);
 Isize=size(Intersection);
 if Isize(1)~=2
     errordlg('Inproper use of function "Twodotintersect", the number of intersection points are not equal to 2!')
 end
-x3 = Intersection(1,:);
-y3 = Intersection(2,:);
+
 
 %% Sequence of intersection points
-angle1=asin(x3(1)/pitagoras(x3(1),y3(1)));
-angle2=asin(x3(2)/pitagoras(x3(2),y3(2)));
-
+angle1=asin(x3(1)/(x3(1)^2+y3(1)^2));
+angle2=asin(x3(2)/(x3(1)^2+y3(1)^2));
 if angle1>angle2
     %[x3(1),y3(1)] is left to [x3(2),y3(2)]
     a=1;
@@ -23,12 +23,10 @@ else
     %[x3(1),y3(1)] is right to [x3(2),y3(2)]
     a=0;
 end
-
-
+%% Compare the lenghts from the Origo
+Lenght=zeros(2,Psize(1)-1);
+comp = zeros(1,Psize(1)-1);
     
-    Lenght=zeros(2,Psize(1)-1);
-    comp = zeros(1,Psize(1)-1);
-    %% Compare the lenghts from the Origo
     for i=1:Psize(1)-1
         Lenght(i,1)=pitagoras(x1(i),x2(i));
         Lenght(i,2)=pitagoras(x1(i),x2(i));
@@ -43,10 +41,10 @@ end
             comp(i)=0;
         end
     end
-    
-    %% Pick non intersecting points
+% Pick non intersecting points
+%% The third point doesn't intersect
     if comp(1)==comp(2)
-        %the third point doesn't intersect
+
         if comp(1)==1
             %the first triangle is larger so the second triangle's first 
             %and second point is needed the first intersection point 
@@ -69,8 +67,9 @@ end
                 end
             end
     end
+%% The first point doesn't intersect
     if comp(2)==comp(3)
-            %the first point doesn't intersect
+
             if comp(2)==1
                 if a==1
                     x4 = [x2(2),x2(3),x3(1),x1(1),x3(2),x2(2)];
@@ -89,8 +88,9 @@ end
                 end
             end
     end
+%% The second point doesn't intersect
     if comp(1)==comp(3)
-        %the second point doesn't intersect
+        
         if comp(2)==1
             if a==1
                 x4 = [x1(3),x1(1),x3(1),x2(2),x3(2),x1(3)];
@@ -109,6 +109,7 @@ end
             end
         end
     end
+%% If there is no two similar points
     if comp(1)~=comp(3) && comp(1)~=comp(2) && comp(2)~=comp(3) 
          errordlg('Inproper use of function "Twodotintersect", calc error!')
     end
--- Geometry/triangle.m
+++ Geometry/triangle.m
@@ -1,16 +1,16 @@
 %Triangle plot and area calculation from tri-phase current parameters
 
-% Ideal parameters
+%% Ideal parameters
 Amplitude_1=[9,9,9];                          %[A]
 Frequency_1=[50,50,50];                        %[Hz]
 Phase_1=[0,2/3*pi,4/3*pi];                     %[rad]
 
-% Real parameters
+%% Real parameters
 Amplitude_2=[5,5,9];                          %[A]
 Frequency_2=[50,50,50];                        %[Hz]
 Phase_2=[0,2/3*pi,4/3*pi]+0.1;                     %[rad]
 
-%Extension of the vectors with the first element to get closed triangles on
+%% Extension of the vectors with the first element to get closed triangles on
 %plots and for use as polygons
 Amplitude_1_ex=[Amplitude_1 Amplitude_1(1)];
 Phase_1_ex=[Phase_1 Phase_1(1)];
@@ -31,11 +31,11 @@ y_3 = Intersection(2,:);
 %haromszogek között, ekkor az eredeti haromszögek pontjait kell használni,
 %bizonys feltételek melett
 
-% 2 metszéspont: meg kell írni!!!
+% 2 metszéspont: meg kell írni!!! ->függvény
 % 1 metszéspont: nem lehet
 % 0 metszéspont: a nagyobból ki kell vonni a kisebbet
 
-% Calculate the areas of the polygons
+%% Calculate the areas of the polygons
 
 A1 = polyarea(x_1,y_1)
 A2 = polyarea(x_2,y_2)
@@ -59,7 +59,7 @@ end
 
 
 
-%Polar plot
+%% Polar plot
 figure
 % polar(Phase_2_ex,Amplitude_2_ex,'r-*')
 % hold on

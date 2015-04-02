cba226c281135b05f5a8d559bea709f9f3661325
Neukirchner László <neukirchner.laszlo@virt.uni-pannon.hu>
2015-04-02 10:56:06 +0200

    Revert "-||-"
    This reverts commit 22cb94c520af0ba0d20decab623ea8da843ee571.

---
+++ Inverter_ini.m
@@ -0,0 +1,32 @@
+% Symmetriser power model initialization
+clear all;
+close all;
+
+%% Simulation initialisation parameters
+CLK=1000;                                           %[sample/s]
+tfin=0.06;                                           %[s]
+sample=10e-5;                                       %[s/sample]
+t=0:sample:tfin;                                    %[tick]
+
+%% Inverter
+Bridge_Inductances=4e-3;
+Current_Comparator_Frequencys=5e-4;
+Buffer_Capacitance=10e-3;
+Buffer_Initial_Voltage=600;
+
+IGBT_Ron=1e-6;
+IGBT_Rs=1e5;
+IGBT_Cs=inf;
+
+
+
+%% Simulation
+        paramNameValStruct.AbsTol         = '1e-9';
+        paramNameValStruct.RelTol         = '1e-9';
+        sim('Inverter');
+
+
+%h = msgbox('Operation Completed','Success');
+
+figure
+plot(Inv_Currents.time,Inv_Currents.signals.values), grid on, legend('R','S','T'), title('Inverter currents')
